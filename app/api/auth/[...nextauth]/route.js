0769ced8d6c52c0e1c8ee8200c6861392383ac66
MS Solanki <mulchandsolanki1122@gmail.com>
2025-05-28 00:40:15 +0530

    Add secret configuration to NextAuth options

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -35,6 +35,8 @@ export const authoptions = NextAuth({
         // }),
     ],
 
+    secret: process.env.NEXTAUTH_SECRET,
+
     callbacks: {
         async signIn({ user, account, profile, email, credentials }) {
             if (account.provider === 'github') {
